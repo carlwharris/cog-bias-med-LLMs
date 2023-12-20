5bf42af4f9723378b3000c70e51f85173a39fe28
carlwharris <carlwharris1@gmail.com>
2023-12-20 16:40:53 -0500

    added in cases where palm2 doesn't respond due to filters

--- evaluate_model.py
+++ evaluate_model.py
@@ -1,14 +1,20 @@
-with open("bias_output_confirmation.txt", "r", encoding='utf8', errors='ignore') as f:
+f_name = "bias_output_text-bison-001.txt"
+with open(f_name, "r", encoding='utf8', errors='ignore') as f:
     lines = f.readlines()
 
 itr = 0
 total = 0
 correct = 0
+refused_to_answer = 0
 for line in lines:
     line = line.replace("\n", "")
+    if "NORESPONSE" in line: refused_to_answer += 1
     if line in ["True", "False"]:
         if line == "True": correct += 1
         total += 1
     itr += 1
-print(correct/total)
+
+print("naive accuracy (where non-response = incorrect)", correct/total)
+print("% refused to answer", refused_to_answer/total)
+print("accuracy (corrected for non-response)", correct/(total - refused_to_answer))
 print(correct,total)
